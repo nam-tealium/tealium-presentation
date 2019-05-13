f067a9023a2b68755333c3b880c2302a49891302
Nam Ninja <namninja@gmail.com>
2019-05-12 23:34:22 -0700

    configuring tealium data layer

--- app/user-router.js
+++ app/user-router.js
@@ -33,12 +33,10 @@ router.get("/login", function(req, res) {
 router.post(
   "/login",
   passport.authenticate("local-login", {
+    successRedirect: "/menu", // redirect to the secure profile section
     failureRedirect: "/login", // redirect back to the signup page if there is an error
     failureFlash: true // allow flash messages
-  }),
-  function(req, res) {
-    res.redirect(`/dashboard/${req.user._id}`); // redirect to the secure profile section
-  }
+  })
 );
 
 // show the signup form
@@ -54,12 +52,10 @@ router.get("/signup", function(req, res) {
 router.post(
   "/signup",
   passport.authenticate("local-signup", {
+    successRedirect: "/menu", // redirect to the secure profile section
     failureRedirect: "/signup", // redirect back to the signup page if there is an error
     failureFlash: true // allow flash messages
-  }),
-  function(req, res) {
-    res.redirect(`/dashboard/${req.user._id}`); // redirect to the secure profile section
-  }
+  })
 );
 
 // LOGOUT ==============================
